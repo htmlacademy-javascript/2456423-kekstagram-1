1df9fc403c8356c5351ec5e05e26f5d567c5fe30
pes0071279 <pes007_1279@mail.ru>
2024-07-18 22:24:45 +0300

    Добавляет обработку вывода комментариев

--- js/create-fullscreen-picture.js
+++ js/create-fullscreen-picture.js
@@ -1,19 +1,47 @@
 import {isKeyEscape} from './util.js';
 
+const COMMENTS_NUMBER = 5;
 const bigPicture = document.querySelector('.big-picture');
 const bigPictureCancel = document.querySelector('.big-picture__cancel');
+const comments = bigPicture.querySelector('.social__comments');
+const commentsLoader = bigPicture.querySelector('.social__comments-loader');
+let commentsCount = 0;
+let Description = null;
+
+const createSotialCommentsCount = (commentsArrayLength) => {
+  const commentsCountText = bigPicture.querySelector('.comments-count').cloneNode();
+  const sotialCommentsCount = bigPicture.querySelector('.social__comment-count');
+  sotialCommentsCount.innerHTML = ' ';
+  sotialCommentsCount.textContent = `${commentsCount} из `;
+  commentsCountText.textContent = commentsArrayLength;
+  sotialCommentsCount.append(commentsCountText);
+};
 
 const createComments = (description) => {
   const commentsListFragment = document.createDocumentFragment();
   const commentsArray = description.comments;
+  const commentsArrayLength = commentsArray.length;
+  const numberAddingComments = commentsArrayLength - commentsCount >= COMMENTS_NUMBER ? COMMENTS_NUMBER : commentsArrayLength - commentsCount;
 
-  commentsArray.forEach((element) => {
+  for(let i = 0; i < numberAddingComments; i++) {
     const comment = document.querySelector('.social__comment').cloneNode(true);
-    comment.querySelector('.social__picture').src = element.avatar;
-    comment.querySelector('.social__picture').alt = element.name;
-    comment.querySelector('.social__text').textContent = element.message;
+    const j = i + commentsCount;
+    comment.querySelector('.social__picture').src = commentsArray[j].avatar;
+    comment.querySelector('.social__picture').alt = commentsArray[j].name;
+    comment.querySelector('.social__text').textContent = commentsArray[j].message;
     commentsListFragment.append(comment);
-  });
+  }
+
+  commentsCount += numberAddingComments;
+
+  createSotialCommentsCount(commentsArrayLength);
+
+  if(commentsCount === commentsArrayLength) {
+    commentsLoader.disabled = true;
+  } else {
+    commentsLoader.disabled = false;
+  }
+
   return commentsListFragment;
 };
 
@@ -28,17 +56,22 @@ const handleCloseButtonClick = () => {
   bigPicture.classList.add('hidden');
   document.body.classList.remove('modal-open');
   document.removeEventListener('keydown', onKeyEscapeKeydown);
+  commentsLoader.removeEventListener('click', onCommentsLoaderClick) ;
+  commentsCount = 0;
+};
+
+const onCommentsLoaderClick = () => {
+  const commentsAdding = createComments(Description);
+  comments.append(commentsAdding);
 };
 
 const openFullScreenImage = (description) => {
+  Description = description;
   bigPicture.querySelector('img').src = description.url;
   bigPicture.querySelector('.likes-count').textContent = description.likes;
   bigPicture.querySelector('.comments-count').textContent = description.comments.length;
   bigPicture.querySelector('.social__caption').textContent = description.description;
-  bigPicture.querySelector('.social__comment-count').classList.add('hidden');
-  bigPicture.querySelector('.comments-loader').classList.add('hidden');
 
-  const comments = bigPicture.querySelector('.social__comments');
   const commentsInner = createComments(description);
   comments.innerHTML = ' ';
   comments.append(commentsInner);
@@ -48,6 +81,7 @@ const openFullScreenImage = (description) => {
   document.addEventListener('keydown', onKeyEscapeKeydown);
 
   bigPictureCancel.addEventListener('click', handleCloseButtonClick);
-};
 
+  commentsLoader.addEventListener('click', onCommentsLoaderClick) ;
+};
 export {openFullScreenImage};
