97bdbd7dbca57e7f20b47096fb8887dbdc5e549c
pes0071279 <pes007_1279@mail.ru>
2024-07-22 23:24:45 +0300

    Вводит правки

--- js/create-fullscreen-picture.js
+++ js/create-fullscreen-picture.js
@@ -3,38 +3,27 @@ import {isKeyEscape} from './util.js';
 const COMMENTS_NUMBER = 5;
 const bigPicture = document.querySelector('.big-picture');
 const bigPictureCancel = document.querySelector('.big-picture__cancel');
-const comments = bigPicture.querySelector('.social__comments');
-const commentsLoader = bigPicture.querySelector('.social__comments-loader');
-let commentsCount = 0;
-let descriptionGlobal = null;
+const bigPictureCommentsElement = bigPicture.querySelector('.social__comments');
+const bigPictureCommentsLoader = bigPicture.querySelector('.social__comments-loader');
+const socialCommentsCount = bigPicture.querySelector('.social__comment-count');
 
-const createComments = (description) => {
-  const commentsCountText = bigPicture.querySelector('.comments-count').cloneNode();
-  const sotialCommentsCount = bigPicture.querySelector('.social__comment-count');
-  const commentsListFragment = document.createDocumentFragment();
-  const commentsArray = description.comments;
-  const commentsArrayLength = commentsArray.length;
-  const numberAddingComments = commentsArrayLength - commentsCount >= COMMENTS_NUMBER ? COMMENTS_NUMBER : commentsArrayLength - commentsCount;
+let commentTemplate;
 
-  for(let i = 0; i < numberAddingComments; i++) {
-    const comment = document.querySelector('.social__comment').cloneNode(true);
-    const j = i + commentsCount;
-    comment.querySelector('.social__picture').src = commentsArray[j].avatar;
-    comment.querySelector('.social__picture').alt = commentsArray[j].name;
-    comment.querySelector('.social__text').textContent = commentsArray[j].message;
-    commentsListFragment.append(comment);
-  }
+let renderedCommentsCount = 0;
+let rawDescription;
 
-  commentsCount += numberAddingComments;
+const createComments = (comments) => {
+  const fragment = document.createDocumentFragment();
 
-  sotialCommentsCount.innerHTML = ' ';
-  sotialCommentsCount.textContent = `${commentsCount} из `;
-  commentsCountText.textContent = commentsArrayLength;
-  sotialCommentsCount.append(commentsCountText);
+  comments.map((comment) => {
+    const commentElement = commentTemplate.cloneNode(true);
+    commentElement.querySelector('.social__picture').src = comment.avatar;
+    commentElement.querySelector('.social__picture').alt = comment.name;
+    commentElement.querySelector('.social__text').textContent = comment.message;
+    fragment.append(commentElement);
+  });
 
-  commentsLoader.disabled = commentsCount === commentsArrayLength;
-
-  return commentsListFragment;
+  return fragment;
 };
 
 const onKeyEscapeKeydown = (evt) => {
@@ -44,36 +33,43 @@ const onKeyEscapeKeydown = (evt) => {
   }
 };
 
-const handleCloseButtonClick = () => {
+const renderComments = () => {
+  bigPictureCommentsElement.append(createComments(rawDescription.comments.slice(renderedCommentsCount, renderedCommentsCount + COMMENTS_NUMBER)));
+  renderedCommentsCount = Math.min(renderedCommentsCount + COMMENTS_NUMBER, rawDescription.comments.length);
+  bigPictureCommentsLoader.disabled = renderedCommentsCount === rawDescription.comments.length;
+  socialCommentsCount.innerHTML = `${renderedCommentsCount} из  <span class="comments-count">${rawDescription.comments.length}</span>`;
+};
+
+const handleCommentsLoaderClick = () => {
+  renderComments();
+};
+
+function handleCloseButtonClick() {
   bigPicture.classList.add('hidden');
   document.body.classList.remove('modal-open');
   document.removeEventListener('keydown', onKeyEscapeKeydown);
-  commentsLoader.removeEventListener('click', onCommentsLoaderClick) ;
-  commentsCount = 0;
-};
+  bigPictureCommentsLoader.removeEventListener('click', handleCommentsLoaderClick) ;
+  renderedCommentsCount = 0;
+}
 
-const onCommentsLoaderClick = () => {
-  const commentsAdding = createComments(descriptionGlobal);
-  comments.append(commentsAdding);
-};
-
-const openFullScreenImage = (description) => {
-  descriptionGlobal = description;
+const openPictureModal = (description) => {
+  rawDescription = description;
   bigPicture.querySelector('img').src = description.url;
   bigPicture.querySelector('.likes-count').textContent = description.likes;
   bigPicture.querySelector('.comments-count').textContent = description.comments.length;
   bigPicture.querySelector('.social__caption').textContent = description.description;
 
-  const commentsInner = createComments(description);
-  comments.innerHTML = ' ';
-  comments.append(commentsInner);
+  commentTemplate = document.querySelector('.social__comment');
+  bigPictureCommentsElement.innerHTML = ' ';
+  renderComments();
 
   bigPicture.classList.remove('hidden');
   document.body.classList.add('modal-open');
   document.addEventListener('keydown', onKeyEscapeKeydown);
 
   bigPictureCancel.addEventListener('click', handleCloseButtonClick);
-
-  commentsLoader.addEventListener('click', onCommentsLoaderClick) ;
+  bigPictureCommentsLoader.addEventListener('click', handleCommentsLoaderClick) ;
 };
-export {openFullScreenImage};
+
+
+export {openPictureModal};
--- js/create-pictures.js
+++ js/create-pictures.js
@@ -1,9 +1,10 @@
-import{openFullScreenImage} from './create-fullscreen-picture.js';
+import{openPictureModal} from './create-fullscreen-picture.js';
 
 const createPictures = function(descriptions) {
   const picturesContainer = document.querySelector('.pictures');
   const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
   const pictureListFragment = document.createDocumentFragment();
+
 
   descriptions.forEach((description) => {
     const picture = pictureTemplate.cloneNode(true);
@@ -12,7 +13,7 @@ const createPictures = function(descriptions) {
     picture.querySelector('.picture__likes').textContent = description.likes;
     picture.dataset.pictureId = description.id;
     picture.addEventListener('click', () => {
-      openFullScreenImage(description);
+      openPictureModal(description);
     });
 
     pictureListFragment.append(picture);
