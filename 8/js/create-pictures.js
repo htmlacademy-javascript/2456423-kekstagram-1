f1b70adc2a0ae114e4778d8d291248581650cb6d
Keks <keks@htmlacademy.ru>
2024-07-31 22:19:55 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/create-pictures.js
@@ -0,0 +1,25 @@
+import{openPictureModal} from './open-picture-modal.js';
+
+const createPictures = function(descriptions) {
+  const picturesContainer = document.querySelector('.pictures');
+  const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+  const pictureListFragment = document.createDocumentFragment();
+
+
+  descriptions.forEach((description) => {
+    const picture = pictureTemplate.cloneNode(true);
+    picture.querySelector('.picture__img').src = description.url;
+    picture.querySelector('.picture__comments').textContent = description.comments.length;
+    picture.querySelector('.picture__likes').textContent = description.likes;
+    picture.dataset.pictureId = description.id;
+    picture.addEventListener('click', () => {
+      openPictureModal(description);
+    });
+
+    pictureListFragment.append(picture);
+
+    picturesContainer.append(pictureListFragment);
+  });
+};
+
+export{createPictures};
---
+++ 8/js/open-form-modal.js
@@ -0,0 +1,70 @@
+import{isKeyEscape} from './util.js';
+
+const HASH_TAGS_ERROR = 'Ошибка ввода хеш-тега';
+const HASH_TAGS_COUNT = 5;
+const VALID_SYMBOLS = /^#[a-zа-яё0-9]{1,19}$/i;
+
+const imgUploadOverlay = document.querySelector('.img-upload__overlay');
+const imgUploadCancel = document.querySelector('.img-upload__cancel');
+const form = document.querySelector('.img-upload__form');
+const hashtagField = document.querySelector('.text__hashtags');
+const commentField = document.querySelector('.text__description');
+
+
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload__field-wrapper__error',
+});
+
+const hasValidCount = (tags) => tags.length <= HASH_TAGS_COUNT && tags.every((tag) => tag.length <= 20);
+
+const hasUniqTags = (tags) => {
+  const lowerCaseTags = tags.map((tag) => tag.toLowerCase());
+  return tags.length === new Set(lowerCaseTags).size;
+};
+
+const hasValidTags = (tags) => tags.every((tag) => VALID_SYMBOLS.test(tag));
+
+const hashTagsValidator = (value) => {
+  const tags = value.trim().split(' ').filter((tag) => tag.trim().length);
+  return hasValidCount(tags) && hasUniqTags(tags) && hasValidTags(tags);
+};
+
+pristine.addValidator(
+  hashtagField,
+  hashTagsValidator,
+  HASH_TAGS_ERROR
+);
+
+const isTextfieldFocused = () => document.activeElement === hashtagField || document.activeElement === commentField;
+
+const onKeyEscapeKeydown = (evt) => {
+  if(isKeyEscape(evt) && !isTextfieldFocused()) {
+    evt.preventDefault();
+    handleCloseButtonClick();
+  }
+};
+
+function handleCloseButtonClick() {
+  form.reset();
+  pristine.reset();
+  imgUploadOverlay.classList.add('hidden');
+  document.querySelector('body').classList.remove('modal-open');
+  document.removeEventListener('keydown', onKeyEscapeKeydown);
+}
+
+const openFormModal = () => {
+  imgUploadOverlay.classList.remove('hidden');
+  document.querySelector('body').classList.add('modal-open');
+
+  document.addEventListener('keydown', onKeyEscapeKeydown);
+  imgUploadCancel.addEventListener('click', handleCloseButtonClick);
+
+  form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    pristine.validate();
+  });
+};
+
+export{openFormModal};
